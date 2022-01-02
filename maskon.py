fa970c9a9c79f6d881a63155f559932e50886054
brettpoland <brett.poland1@gmail.com>
2022-01-02 07:44:38 -0700

    1st Commit

--- maskon.py
+++ maskon.py
@@ -2,24 +2,30 @@ import subprocess as sub
 import argparse 
 import re
 import random
-
-interface = 'en1'
-
+import platform
+import uuid
+#test
 def get_args():
+    # Create the parser
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--interface', dest = 'interface', help = 'Interface name to change mac')
-    option = parser.parse_args()
+    # Add an argument
+    parser.add_argument('--interface', type=str, required=True)
+    # Parse the argument
+    args = parser.parse_args()
+    return args.interface
 
-    if not option.interface:
-        parser.error("Please specify an interface in the arguments, use --help for more info")
-    return option
-
+#"Windows", "Linux", "Mac?"
+def os_detection():
+    my_os = platform.system()
+    return my_os
+    
 #Randomize new mac address
-new_mac = "02:00:00:%02x:%02x:%02x" % (random.randint(0, 255), random.randint(0, 255),random.randint(0, 255))
-
+def new_mac(mac):
+    new_mac = "02:00:00:%02x:%02x:%02x" % (random.randint(0, 255), random.randint(0, 255),random.randint(0, 255))
+    return new_mac
 
 #Get Current mac to compare
-def get_current_mac(interface):
+def get_current_mac_unix(interface):
   output = sub.check_output(['ifconfig', interface], universal_newlines = True)
   search_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", output)
   if search_mac:
@@ -29,17 +35,28 @@ def get_current_mac(interface):
   else:
     print('[-] Could not read the MAC Address')
 
-def maskon(interface, new_mac):
+def get_current_mac_windows():
+    mac_num = hex(uuid.getnode()).replace('0x', '').upper()
+    mac = '-'.join(mac_num[i: i + 2] for i in range(0, 11, 2))
+    return mac
+
+def maskon_unix(interface, new_mac):
     print("\n Putting Mask On \n")
     sub.call(['sudo', 'ifconfig', interface, 'ether', new_mac])
 
+if __name__ == "__main__":
+    interface = get_args()
+    os = os_detection()
+    if os == "Windows":
+        print("Windows OS Not Supported")
+        
+    else:
+        mac = get_current_mac_unix(interface)
+        new_mac = new_mac(mac) 
+        maskon_unix(interface, new_mac)
 
-mac = get_current_mac(interface)
-
-maskon(interface, new_mac)
-
-if mac != new_mac:
-    print('MAC has been changed')
-    print('New mac is ' + new_mac)
-else:
-    print('Could not change MAC')
+        if mac != new_mac:
+            print('MAC has been changed')
+            print('New mac is ' + new_mac)
+        else:
+            print('Could not change MAC')
